278111905bafdc8805b35db4947b0af77766bc22
Chun Lo <chunlohk@gmail.com>
2023-03-25 02:13:43 -0400

    Update PrivacyModalAgreeTest.js into test folder for future github acton reference

--- tests/PrivacyModalAgreeTest.js
+++ tests/PrivacyModalAgreeTest.js
@@ -1,29 +1,6 @@
-const { chromium } = require('playwright');
+import { test, expect } from '@playwright/test';
 
-(async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-
-  // Replace 'http://localhost:3000' with your website's URL
-  await page.goto('http://localhost:3000');
-
-  // Wait for the privacy modal to appear
-  await page.waitForSelector('#privacyModal');
-
-  // Click the "I Agree" button
-  await page.click('#agreeButton');
-
-  // Check if the privacy modal is hidden
-  const isModalVisible = await page.$eval('#privacyModal', (modal) => {
-    return modal.classList.contains('show');
-  });
-
-  if (!isModalVisible) {
-    console.log('The "I Agree" button is working correctly.');
-  } else {
-    console.error('The "I Agree" button is not working.');
-  }
-
-  await browser.close();
-})();
+test('test', async ({ page }) => {
+  await page.goto('https://chunlohk.github.io/mywebclass-simulation/');
+  await page.getByRole('button', { name: 'I Agree' }).click();
+});
